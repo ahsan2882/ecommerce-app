4b576b6d35e09df87050bf2e9f66c0a2ed2ea2ff
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-01-19 10:47:12 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from "react";
+import { BrowserRouter as BRouter, Route, Routes, useNavigate } from "react-router-dom";
+import HomePage from "./Components/HomePage/HomePage";
+import LoginContainer from "./Components/LoginSignup/LoginContainer";
+
+
 function App() {
+  let navigate = useNavigate();
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={<LoginContainer page={'login'} />} />
+          <Route path='/signup' element={<LoginContainer page={'signup'} />} />
+        </Routes>
+      </BRouter>
+    </>
   );
 }
 
