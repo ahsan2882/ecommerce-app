b908c1998e0af36505686965876eea043c5399bd
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-01-19 19:03:11 +0500

    Update LoginContainer.js

--- src/Components/LoginSignup/LoginContainer.js
+++ src/Components/LoginSignup/LoginContainer.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import Login from './Login'
-import Signup from './Signup'
+import React from "react";
+import PropTypes from "prop-types";
+import Login from "./Login";
+import Signup from "./Signup";
 
 export default function LoginContainer({page}) {
-    return (
-        <>
-            <section className='w-full h-screen'>
-                <section className={`w-full h-full flex items-center ${(page === 'login') ? 'bg-gradient-to-br from-indigo-300 via-violet-500 to-purple-800' : 'bg-gradient-to-tr from-fuchsia-500 via-purple-400 to-violet-400'}`}>
-                    {page === 'login' ? <Login/> : <Signup/>}
-                </section>
-            </section>
-        </>
-    )
+	return (
+		<>
+			<section className='w-full h-screen'>
+				<section className={`w-full h-full flex items-center ${(page === "login") ? "bg-gradient-to-br from-indigo-300 via-violet-500 to-purple-800" : "bg-gradient-to-tr from-fuchsia-500 via-purple-400 to-violet-400"}`}>
+					{page === "login" ? <Login/> : <Signup/>}
+				</section>
+			</section>
+		</>
+	);
 }
+
+
+LoginContainer.propTypes = {
+	page: PropTypes.string.isRequired
+};
