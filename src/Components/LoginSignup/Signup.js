3d0802707acc77cd402bb08cf6516fcff431d254
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-01-19 10:47:34 +0500

    Create Signup.js

---
+++ src/Components/LoginSignup/Signup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Signup() {
+    return (
+        <>
+            <section className='w-full h-screen'>
+
+            </section>
+        </>
+    )
+}
