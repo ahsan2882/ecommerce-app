7f46e3207242e7e242bfdeaae3308b214ff8af2b
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-01-19 19:03:03 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-unused-vars */
+
 import React, { useEffect } from "react";
 import { BrowserRouter as BRouter, Route, Routes, useNavigate } from "react-router-dom";
 import HomePage from "./Components/HomePage/HomePage";
@@ -6,27 +6,28 @@ import LoginContainer from "./Components/LoginSignup/LoginContainer";
 
 
 function App() {
-  let navigate = useNavigate();
-  useEffect(() => {
+	// let navigate = useNavigate();
+	// useEffect(() => {
     
-    if (localStorage.getItem('authenticated') === null || localStorage.getItem('authenticated') === 'false') {
-      localStorage.setItem('authenticated', 'false');
-      navigate('/login');
-    } else {
-      navigate('/');
-    }
-  }, [navigate])
-  return (
-    <>
-      <BRouter>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/login' element={<LoginContainer page={'login'} />} />
-          <Route path='/signup' element={<LoginContainer page={'signup'} />} />
-        </Routes>
-      </BRouter>
-    </>
-  );
+	//   if (localStorage.getItem('authenticated') === null || localStorage.getItem('authenticated') === 'false') {
+	//     localStorage.setItem('authenticated', 'false');
+	//     navigate('/login');
+	//   } else {
+	//     navigate('/');
+	//     // if(new Date() - parseInt(localStorage.getItem('loginTime')))
+	//   }
+	// }, [navigate])
+	return (
+		<>
+			<BRouter>
+				<Routes>
+					<Route path='/' element={<HomePage />} />
+					<Route path='/login' element={<LoginContainer page={"login"} />} />
+					<Route path='/signup' element={<LoginContainer page={"signup"} />} />
+				</Routes>
+			</BRouter>
+		</>
+	);
 }
 
 export default App;
