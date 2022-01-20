822a38477ebdbd68ac24ca406c4f11fc193d017d
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-01-20 13:32:28 +0500

    Create LoggedOut.js

---
+++ src/Components/LoginSignup/LoggedOut.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LoggedOut() {
+    return (
+        <>
+            <h1 className="text-5xl font-medium text-center">You have been logged out!</h1>
+        </>
+    );
+}
+
+export default LoggedOut;
