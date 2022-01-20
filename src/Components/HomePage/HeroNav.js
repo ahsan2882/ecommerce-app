ff0cc1ad2dc4f530a34746e6270c8f3792214eca
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-01-20 19:45:01 +0500

    Create HeroNav.js

---
+++ src/Components/HomePage/HeroNav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function HeroNav() {
+    return (
+        <>
+            <section className="w-full bg-sky-300">
+                <section className="max-w-6xl mx-auto w-full">
+
+                </section>
+            </section>
+        </>
+    );
+}
