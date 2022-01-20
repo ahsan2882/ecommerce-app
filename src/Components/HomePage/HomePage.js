a894fc1935b22d08d1557c137c6697fdc91f544d
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-01-20 19:45:05 +0500

    Update HomePage.js

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import { authCheck } from "../authCheck";
+import Header from "./Header";
 
 export default function HomePage() {
 	let location = useLocation();
+
 	authCheck(location.pathname);
 	return (
 		<>
-			<h1>This is the homepage</h1>
+			<Header />
 		</>
 	);
 }
