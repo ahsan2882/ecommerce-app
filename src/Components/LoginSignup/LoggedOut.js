94a2115d35939f03179fab6b1afc6ca3674de05d
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-01-20 19:45:08 +0500

    Update LoggedOut.js

--- src/Components/LoginSignup/LoggedOut.js
+++ src/Components/LoginSignup/LoggedOut.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function LoggedOut() {
     return (
         <>
-            <h1 className="text-5xl font-medium text-center">You have been logged out!</h1>
+            <section>
+                <h1 className="text-5xl font-medium text-center">You have been logged out!</h1>
+                <Link to='/login' />
+           </section>
+
         </>
     );
 }
