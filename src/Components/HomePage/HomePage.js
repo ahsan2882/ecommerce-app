980dd8ae6267afbdce6013c39e339ca158d1cd94
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-01-20 10:50:26 +0500

    Update HomePage.js

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
+
+	let navigate = useNavigate();
+	useEffect(() => {
+		let authStatus = localStorage.getItem("authenticated");
+		if (authStatus === null || authStatus === "false") {
+			navigate("/login");
+			authStatus === null && localStorage.setItem("authenticated", "false");
+		}
+	}, []);
 	return (
 		<>
 			<h1>This is the homepage</h1>
