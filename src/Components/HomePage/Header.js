87dca095fcb47e8a6e98d01a63f950a563aa496c
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-01-24 19:47:20 +0500

    Update Header.js

--- src/Components/HomePage/Header.js
+++ src/Components/HomePage/Header.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import logo from "../../assets/images/navLogo.png";
 
 export default function Header() {
     let navigate = useNavigate();
@@ -15,21 +14,19 @@ export default function Header() {
                 <nav className="max-w-6xl w-full mx-auto mt-12 mb-5" aria-label="search navigation">
                     <ul className="navBar flex items-center justify-between w-full">
                         <li className="w-1/6">
-                            <Link to='/'><img src={logo} alt="siteLogo" className="h-9"/></Link>
+                            <Link to='/'><h1 className="text-2xl uppercase text-indigo-400">e-shop</h1></Link>
                         </li>
                         <li className="w-1/2">
                             <div className="flex items-stretch">
-                                <input type="search" name="search" id="searchProduct" className="w-11/12 px-4 py-2 outline-none bg-gray-100" placeholder="Search in Daraz" />
-                                <button className="w-1/12 bg-orange-500"><i className="fas fa-search"></i></button>
+                                <input type="search" name="search" id="searchProduct" className="w-11/12 px-4 py-2 outline-none bg-gray-100" placeholder="Search in store" />
+                                <button className="w-1/12 bg-indigo-500"><i className="fas fa-search"></i></button>
                             </div>
                         </li>
                         <li className="w-1/12">
                             <Link to='/cart'><i className="fas fa-shopping-cart text-lg"></i></Link>
                         </li>
                         <li className="w-1/6">
-                            <div>
-                                Get mobile app
-                            </div>
+                            <Link to='/logout'>Logout</Link>
                         </li>
                     </ul>
                 </nav>
