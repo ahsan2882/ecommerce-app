bf170f3aead63603e1e00cf12f813aca2add8b4c
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-01-24 10:33:24 +0500

    Update Header.js

--- src/Components/HomePage/Header.js
+++ src/Components/HomePage/Header.js
@@ -12,39 +12,14 @@ export default function Header() {
     return (
         <>
             <header className="w-full">
-                <nav className="w-7/12 mx-auto my-3" aria-label="main navigation">
-                    <ul className="topNav flex items-center justify-evenly w-full">
-                        <li className="topNavItem uppercase">
-                            <h3>Save more on app</h3>
-                        </li>
-                        <li className="topNavItem uppercase">
-                            <h3>Daraz Affiliate program</h3>
-                        </li>
-                        <li className="topNavItem uppercase">
-                            <h3>Sell on daraz</h3>
-                        </li>
-                        <li className="topNavItem uppercase">
-                            <h3>Customer Care</h3>
-                        </li>
-                        <li className="topNavItem uppercase">
-                            <h3>Track my order</h3>
-                        </li>
-                        <li className="topNavItem uppercase">
-                            <Link to='/login'>Login</Link>
-                        </li>
-                        <li className="topNavItem uppercase">
-                            <Link to='/signup'>Signup</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <nav className="max-w-6xl w-full mx-auto my-3 mb-5" aria-label="search navigation">
+                <nav className="max-w-6xl w-full mx-auto mt-12 mb-5" aria-label="search navigation">
                     <ul className="navBar flex items-center justify-between w-full">
                         <li className="w-1/6">
                             <Link to='/'><img src={logo} alt="siteLogo" className="h-9"/></Link>
                         </li>
                         <li className="w-1/2">
                             <div className="flex items-stretch">
-                                <input type="search" name="search" id="searchProduct" className="w-11/12 px-4 py-2 outline-none bg-gray-200" placeholder="Search in Daraz" />
+                                <input type="search" name="search" id="searchProduct" className="w-11/12 px-4 py-2 outline-none bg-gray-100" placeholder="Search in Daraz" />
                                 <button className="w-1/12 bg-orange-500"><i className="fas fa-search"></i></button>
                             </div>
                         </li>
