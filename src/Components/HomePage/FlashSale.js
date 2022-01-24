bfcc05562870066ac41b92e19706c1482e57c79b
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-01-24 19:47:15 +0500

    Update FlashSale.js

--- src/Components/HomePage/FlashSale.js
+++ src/Components/HomePage/FlashSale.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function FlashSale() {
+    const [flashItems, setFlashItems] = useState([]);
+    useEffect(() => {
+        fetch("https://fakestoreapi.com/products?limit=5")
+            .then(res => res.json())
+            .then(json => setFlashItems(json))
+            .catch(error => console.log(error));
+    }, []);
     return (
         <>
             <section className="max-w-6xl w-full mx-auto py-8">
@@ -8,14 +16,20 @@ export default function FlashSale() {
                 <section className="bg-white py-4 flex flex-col">
                     <section className="flex items-center justify-between px-4 border-b border-b-gray-500 pb-3">
                         <h1>On Sale Now</h1>
-                        <button className="border border-orange-500 text-orange-500 px-2 py-1 uppercase">Shop More</button>
+                        <button className="border border-indigo-500 text-indigo-500 px-2 py-1 uppercase">Shop More</button>
                     </section>
-                    <section className="flex items-center">
-                        {/* {
-                            fetch("https://fakestoreapi.com/products?limit=5")
-                                .then(res => res.json())
-                                .then(json => console.log(json))
-                        } */}
+                    <section className="flex items-center justify-around">
+                        {flashItems.map(item => (
+                            <Link key={item.id} to={`/products/${item.id}`} className="flex flex-col w-1/6 items-start shadow-2xl">
+                            <div className="w-full">
+                                <img src={item.image} alt={item.description.substring(0, 16)} className="w-full h-48 object-contain mb-6 pb-4 border-b" />
+                                <h1 className="px-4">{ item.title.substring(0, 16)}...</h1>
+                                <div className="flex items-center px-4">
+                                    <h1>${item.price}</h1>
+                                </div>
+                                </div>
+                            </Link>
+                        ))}
                     </section>
                 </section>
             </section>
