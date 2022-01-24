7ca497d7358aa72fdb34c5b5838ccef37f63b67a
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-01-24 10:33:28 +0500

    Update HeroNav.js

--- src/Components/HomePage/HeroNav.js
+++ src/Components/HomePage/HeroNav.js
@@ -4,10 +4,10 @@ import HNavCss from "./HeroNav.module.css";
 export default function HeroNav() {
     return (
         <>
-            <section className="w-full bg-sky-300">
+            <section className="w-full bg-indigo-300">
                 <section className="max-w-6xl mx-auto w-full">
-                    <nav aria-label="products navigation" className="flex w-1/5">
-                        <ul className="flex flex-col items-start justify-evenly py-1 w-full bg-white">
+                    <nav aria-label="products navigation" className="flex w-1/5 shadow">
+                        <ul className="flex flex-col items-start justify-evenly py-1 w-full bg-gray-200">
                             {[                               
                                 { mainLi: "Electronic Devices", subLi: ["Smart Phones", "Feature Phones", "Tablets", "Landline Phones", "Laptops", "Desktops", "Smart Watches", "Gaming Consoles", "Cameras & Drones", "Security Cameras", "Insurance and Protection", "Renewed"] },
                                 { mainLi: "Electronic Accessories", subLi: ["Mobile Accessories", "Headphones & Headsets", "Wearable", "Camera Accessories", "Computer Accessories", "Storage", "Printers", "Computer Components", "Network Components", "Portable Speakers"] },
@@ -20,16 +20,16 @@ export default function HeroNav() {
                                 { mainLi: "Electronic Devices", subLi: ["Smart Phones", "Feature Phones", "Tablets", "Landline Phones", "Laptops", "Desktops", "Smart Watches", "Gaming Consoles", "Cameras & Drones", "Security Cameras", "Insurance and Protection", "Renewed"] },
                                 { mainLi: "Electronic Accessories", subLi: ["Mobile Accessories", "Headphones & Headsets", "Wearable", "Camera Accessories", "Computer Accessories", "Storage", "Printers", "Computer Components", "Network Components", "Portable Speakers"] }
                             ].map(item => (
-                                <li key={item.mainLi.toLowerCase().replace(" ", "")} className={`px-2 py-1 text-sm hover:cursor-pointer hover:bg-gray-300 hover:text-blue-500 w-full flex items-center justify-between relative ${HNavCss.sideNavLi}`} >
+                                <li key={(item.mainLi.toLowerCase().replace(" ", "").concat(`${Math.ceil(Math.random()*100000)}`))} className={`px-2 py-1 text-sm hover:cursor-pointer hover:bg-gray-300 hover:text-blue-500 w-full flex items-center justify-between relative ${HNavCss.sideNavLi}`} >
                                     <h4>{item.mainLi}</h4>
-                                    <div className={HNavCss.icon}><i className={HNavCss.subList}></i></div>
-                                    <ul className="absolute -right-full bg-white">
+                                    <div className={HNavCss.icon}><i className="fas fa-chevron-right"></i></div>
+                                    {/* <ul className="absolute -right-full hidden bg-white">
                                         {item.subLi.map(items => (
                                             <li key={items.toLowerCase().replace(" ", "")}>
                                                 {items}
                                             </li>
                                         ))}
-                                    </ul>
+                                    </ul> */}
                                 </li>)
                             )}
                         </ul>
