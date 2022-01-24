ddd9f44a6a36b72d8aa0f20e6fd458e961ea4671
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-01-24 10:33:40 +0500

    Create ProductSection.js

---
+++ src/Components/HomePage/ProductSection.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FlashSale from "./FlashSale";
+
+export default function ProductSection() {
+    return (
+        <>
+            <section className="bg-gray-100 py-6">
+                <FlashSale />
+            </section>
+        </>
+    );
+}
