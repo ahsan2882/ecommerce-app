edf82c2bafc835c2a3c5d56878dc6aa08608bbf2
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-01-25 15:02:30 +0500

    Update ProductCategories.js

---
+++ src/Components/HomePage/ProductCategories.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function ProductCategories() {
+    return (
+        <>
+            <section className="max-w-6xl w-full mx-auto py-8">
+                <h1 className="text-xl text-gray-500 mb-4">Categories</h1>
+            </section>
+        </>
+    );
+}
