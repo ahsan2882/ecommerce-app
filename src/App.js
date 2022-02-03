ec2f6b7952f4a9cf7614d55d6b5ca41864522a7e
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-02-03 15:33:37 +0500

    link to redux store created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 
 import React from "react";
+import { Provider } from "react-redux";
 import { BrowserRouter as BRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./Components/HomePage/HomePage";
 import ProductDetail from "./Components/HomePage/ProductDetail";
 import LoggedOut from "./Components/LoginSignup/LoggedOut";
 import LoginContainer from "./Components/LoginSignup/LoginContainer";
 import NotFound from "./Components/NotFound";
+import store from "./Components/reduxStore/store";
 
 function App() {
 	return (
 		<>
-			<BRouter>
-				<Routes>
-					<Route path='/' element={<HomePage />} />
-					<Route path='/login' element={<LoginContainer page={"login"} />} />
-					<Route path='/signup' element={<LoginContainer page={"signup"} />} />
-					<Route path='/logout' element={<LoggedOut />} />
-					<Route path='/products/:productId' element={<ProductDetail />} />
-					<Route path="*" element={<NotFound />} />
-				</Routes>
-			</BRouter>
+			<Provider store={store}>
+				<BRouter>
+					<Routes>
+						<Route path='/' element={<HomePage />} />
+						<Route path='/login' element={<LoginContainer page={"login"} />} />
+						<Route path='/signup' element={<LoginContainer page={"signup"} />} />
+						<Route path='/logout' element={<LoggedOut />} />
+						<Route path='/products/:productId' element={<ProductDetail />} />
+						<Route path="*" element={<NotFound />} />
+					</Routes>
+				</BRouter>
+			</Provider>
 		</>
 	);
 }
