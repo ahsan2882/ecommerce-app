d2f45b954d46a4479f8eb869258aba8df53ba7a3
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-02-03 15:36:00 +0500

    Update ProductSection.js

--- src/Components/HomePage/ProductSection.js
+++ src/Components/HomePage/ProductSection.js
@@ -1,13 +1,13 @@
 import React from "react";
 import FlashSale from "./FlashSale";
-// import ProductCategories from "./ProductCategories";
+import ProductCategories from "./ProductCategories";
 
 export default function ProductSection() {
     return (
         <>
             <section className="bg-gray-100 py-6">
                 <FlashSale />
-                {/* <ProductCategories /> */}
+                <ProductCategories />
             </section>
         </>
     );
