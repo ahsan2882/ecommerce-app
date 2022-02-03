a2e7a6b89b522d6b74ca503a2e35037a3c333a7e
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-02-03 15:35:52 +0500

    Update ProductDetail.js

--- src/Components/HomePage/ProductDetail.js
+++ src/Components/HomePage/ProductDetail.js
@@ -1,31 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
+import useFetch from "../customHooks/useFetch";
 import Header from "./Header";
 
 export default function ProductDetail() {
     const { productId } = useParams();
-    const [productInfo, setProductInfo] = useState({});
-    console.log(productId);
-    useEffect(() => {
-        fetch(`http://localhost:8000/single-product/${productId}`)
-            .then(res => res.json())
-            .then(json => setProductInfo(json))
-            .catch(error => console.log(error));
-    }, []);
+    const { data: productInfo, isLoading, error } = useFetch(`http://localhost:8000/single-product/${productId}`);
     return (
         <>
             <Header />
             <section className="w-full bg-gray-200 py-6">
                 <section className="max-w-6xl w-full mx-auto bg-white py-12 flex items-stretch justify-evenly">
-                    <img src={productInfo.image} alt={productInfo.title} className="w-2/5 h-96 object-contain"/>
-                    <section className="flex flex-col items-start justify-around w-2/5">
-                        <h1 className="text-4xl py-4">{productInfo.title}</h1>
-                        <p>{productInfo.description}</p>
-                        <div className="flex items-center justify-between w-full">
-                            <p className="text-xl">$&nbsp;{productInfo.price}</p>
-                            <p></p>
-                        </div>
-                    </section>
+                    {error && <h1>{error}</h1>}
+                    {isLoading && <h1>Loading....</h1>}
+                    {productInfo &&
+                        <>
+                            <img src={productInfo.image} alt={productInfo.title} className="w-2/5 h-96 object-contain" />
+                            <section className="flex flex-col items-start justify-around w-2/5">
+                                <h1 className="text-4xl py-4">{productInfo.title}</h1>
+                                <p>{productInfo.description}</p>
+                                <div className="flex items-center justify-between w-full">
+                                    <p className="text-xl">$&nbsp;{productInfo.price}</p>
+                                    <p></p>
+                                </div>
+                            </section>
+                        </>
+                    }
                 </section>
             </section>
         </>
